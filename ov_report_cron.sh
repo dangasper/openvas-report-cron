7a0a663ca9597c1af20bc0f214d53406d741a795
Daniel Gasper <dangasper@gmail.com>
2018-08-05 19:19:50 -0400

    Adding in new report pull functions

--- ov_report_cron.sh
+++ ov_report_cron.sh
@@ -4,15 +4,17 @@
 # Constant variables
 USER=""
 PASS=""
+HOST=""
 RETURNSTATUS=""
 TODAY=$(date +%Y-%m-%d)
 REPORTLOC=""
 REPORTDB=""
+PROCESSEDLOC=""
 
 # Test OMP connection - uses OMP ping exits script on failure
 test_omp() {
 	echo "----------- TESTING OMP CONNECTION -------"
-	if [ ! "eval omp -u $USER -w $PASS --ping" ]
+	if [ ! "eval omp -h $HOST -u $USER -w $PASS --ping" ]
 	then
 		echo "OMP Connection Failed. Exiting script..."
 		exit 1
@@ -24,18 +26,67 @@ test_omp() {
 # Get tasks function - polls OMP for tasks and stores in an array variable
 get_tasks() {
 	echo "----------- GETTING TASKS ----------"
-	TaskID=(`omp -u $USER -w $PASS -G | awk '{ print $1 }'`)
+	TaskID=(`omp -h $HOST -u $USER -w $PASS -G | awk '{ print $1 }'`)
 	echo "Tasks received"
 }
 
 # report parse function - gets task information from passed task id of get_reports() and parses through output to grab last report ID
 report_parse() {
-	omp -u $USER -w $PASS -iX '<get_tasks task_id="'${1}'"></get_tasks>' | sed -n '/<last_report>/,/<\/last_report>/p' | grep "report id" | awk -F\" '{ print $2 }'
+	omp -h $HOST -u $USER -w $PASS -iX '<get_tasks task_id="'${1}'"></get_tasks>' | sed -n '/<last_report>/,/<\/last_report>/p' | grep "report id" | awk -F\" '{ print $2 }'
 }
 
-# Get reports function - polls OMP using tasks to find last report id
+# Creates reportdb function - creates report db file
+create_reportdb() {
+	echo ${ReportID[@]} > $REPORTDB
+}
+
+locate_reportdb() {
+	[ -f "$REPORTDB" ]
+}
+
+pull_new_reports() {
+	echo "Pulling new reports."
+	echo ${NewReportID[@]}
+	echo "New reports pulled."
+	# Update DB file with current reports pulled.
+	echo ${ReportID[@]} > $REPORTDB
+}
+
+# new reports function - checks if the reportdb file is available, creates if not, if present then compares report database file against pulled report ids, calls new report pull function
+new_reports() {
+	if locate_reportdb
+	then
+		echo "Report DB found, looking for new reports."
+		newreports=0
+		for j in ${ReportID[@]}
+		do
+			if [ ! `grep -c $j $REPORTDB` -ge 1 ]
+			then
+				NewReportID[$newreports]=$j
+				newreports=$[$newreports+1]
+			fi		
+		done
+		if [ ${newreports} -ge 1 ]
+		then
+			echo "$newreports new reports found!"
+			pull_new_reports
+		else
+			echo "No new reports found!"
+		fi
+	else
+		create_reportdb
+		echo "Report DB created."
+		echo "`echo ${ReportID[@]} | wc -w` new reports found!"
+		NewReportID=(${ReportID[@]})
+		pull_new_reports
+	fi
+	
+}
+
+# Get reports function - polls OMP using tasks to find last report id, compares to previous pulled reports, grabs new reports
 get_reports() {
 	echo "----------- GETTING REPORTS -----------"
+	
 	c="0"
 	for i in ${TaskID[@]}
 	do
@@ -44,6 +95,13 @@ get_reports() {
 	done
 	echo "Reports have been received"
 
+	# Check for reports folder before continuing
+	if [ ! -d "$REPORTLOC" ]
+	then
+		mkdir -p $(pwd)/reports/
+	fi
+
+	new_reports
 }
 
 # Main script start
